39662831d0918d234eaaff2782fe872ffa8e9054
HowardQian201 <75398680+HowardQian201@users.noreply.github.com>
2021-08-03 22:06:43 -0400

    Add files via upload

--- content_classification.py
+++ content_classification.py
@@ -2,9 +2,11 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report, confusion_matrix
 import json, pandas
 from nltk.stem.snowball import SnowballStemmer
-
+import random
+import numpy as np
 
 
 
@@ -59,4 +61,59 @@ def createSklearnModel(training_headlines, training_categories):
     gs_clf_svm = gs_clf_svm.fit(training_headlines, training_categories)
 
     return gs_clf_svm
+
+
+def writeToCSV(filename, predicted_categories):
+    df = pandas.read_csv(filename)
+    df['Category'] = predicted_categories
+    print(df)
+    df.to_csv(filename)
+
+
+def trainingAccuracyTest(training_headlines, training_categories):
+    #list of unique categories
+    unique_categories = list(set(training_categories))
+
+    #shuffle randomly
+    shuffled = list(zip(training_headlines, training_categories))
+    random.shuffle(shuffled)
+    headlines, categories = zip(*shuffled)
+
+    #split data
+    training_head = headlines[:199600]
+    training_cat = categories[:199600]
+    test_head = headlines[199600:]
+    test_cat = categories[199600:]
+
+    #create model and predict
+    model = createSklearnModel(training_head, training_cat)
+    predicted_categories = model.predict(test_head)
+
+    #performance metrics
+    report = classification_report(test_cat, predicted_categories, output_dict=True, target_names=unique_categories)
+    df = pandas.DataFrame(report).transpose()
+    df.to_csv('/Users/howardqian/Desktop/ML and CNN/corsali-machine-learning-technical-interview-main/performance_metrics.csv')
+    print(df)
+
+    #confusion matrix
+    unique_label = np.unique([test_cat, predicted_categories])
+    cmtx = pandas.DataFrame(
+        confusion_matrix(test_cat, predicted_categories, labels=unique_label),
+        index=['true:{:}'.format(x) for x in unique_label],
+        columns=['pred:{:}'.format(x) for x in unique_label]
+    )
+    cmtx.to_csv('/Users/howardqian/Desktop/ML and CNN/corsali-machine-learning-technical-interview-main/confusion_matrix.csv')
+    print(cmtx)
+
+
+
+
+
+
+
+
+
+
+
+
 
